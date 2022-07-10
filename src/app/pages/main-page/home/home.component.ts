fcfe450db4aaf53dafe0d92f61dd6ec420d42829
Ricardo Mantilla <mantillasanchezr@gmail.com>
2022-07-10 01:58:32 -0500

    Se trae la informacion de modelos desde la bd

--- src/app/pages/main-page/home/home.component.ts
+++ src/app/pages/main-page/home/home.component.ts
@@ -1,3 +1,5 @@
+import { Imodels } from './../../../interface/imodels';
+import { ModelsService } from './../../../services/models.service';
 import { IQueryParams } from './../../../interface/i-query-params';
 import { FontAwesomeIconsService } from './../../../shared/font-awesome-icons/font-awesome-icons.service';
 import { Icategories } from './../../../interface/icategories';
@@ -11,14 +13,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   public categories: Icategories[] | null = [];
+  public models: Imodels[] | null = [];
 
   constructor(
     private categoriesService: CategoriesService,
+    private modelsService: ModelsService,
     public fontAwesomeIconsService: FontAwesomeIconsService
   ) {}
 
   ngOnInit(): void {
     this.getAllCategories();
+    this.getAllModels();
   }
 
   public getAllCategories(): void {
@@ -30,6 +35,17 @@ export class HomeComponent implements OnInit {
     this.categoriesService.getData(queryParams).subscribe(
       (res: Icategories[]) => {
         this.categories = res;
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
+
+  public getAllModels(): void {
+    this.modelsService.getData().subscribe(
+      (res: Imodels[]) => {
+        this.models = res;
       },
       (error) => {
         console.error(error);
