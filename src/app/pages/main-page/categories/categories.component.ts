24428b5cfd4a6cb4225e50bbcf7305c33aea3135
Ricardo Mantilla <mantillasanchezr@gmail.com>
2022-07-10 01:21:40 -0500

    Se añaden las categorias en la pagina de categorias

--- src/app/pages/main-page/categories/categories.component.ts
+++ src/app/pages/main-page/categories/categories.component.ts
@@ -1,15 +1,33 @@
+import { FontAwesomeIconsService } from './../../../shared/font-awesome-icons/font-awesome-icons.service';
+import { CategoriesService } from './../../../services/categories.service';
+import { Icategories } from './../../../interface/icategories';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
-  styleUrls: ['./categories.component.css']
+  styleUrls: ['./categories.component.css'],
 })
 export class CategoriesComponent implements OnInit {
+  public categories: Icategories[] | null = [];
 
-  constructor() { }
+  constructor(
+    private categoriesService: CategoriesService,
+    public fontAwesomeIconsService: FontAwesomeIconsService
+  ) {}
 
   ngOnInit(): void {
+    this.getAllCategories();
   }
 
+  public getAllCategories(): void {
+    this.categoriesService.getData().subscribe(
+      (res: Icategories[]) => {
+        this.categories = res;
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
 }
