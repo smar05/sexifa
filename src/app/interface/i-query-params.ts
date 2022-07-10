a25d64ecefe5a0475408ae6d2a82b0f003aeca80
Ricardo Mantilla <mantillasanchezr@gmail.com>
2022-07-09 19:44:47 -0500

    Servicio comun para las peticiones http a la base de datos

---
+++ src/app/interface/i-query-params.ts
@@ -0,0 +1,8 @@
+export interface IQueryParams {
+  orderBy?: string; //Ordenar por un parametro de la coleccion
+  startAt?: string;
+  endAt?: string;
+  equalTo?: string;
+  limitToFirst?: number;
+  limitToLast?: number;
+}
