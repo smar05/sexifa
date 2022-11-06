e8813b5683732d6cf770dc4158c95dde44e68eb8
Ricardo Mantilla <mantillasanchezr@gmail.com>
2022-11-05 21:10:10 -0500

    Datos del usuario

--- src/app/pages/main-page/checkout/checkout.component.ts
+++ src/app/pages/main-page/checkout/checkout.component.ts
@@ -1,15 +1,31 @@
+import { Iuser } from './../../../interface/iuser';
+import { IQueryParams } from './../../../interface/i-query-params';
+import { UserService } from './../../../services/user.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
-  styleUrls: ['./checkout.component.css']
+  styleUrls: ['./checkout.component.css'],
 })
 export class CheckoutComponent implements OnInit {
+  public user: Iuser = {};
 
-  constructor() { }
+  constructor(private userService: UserService) {}
 
   ngOnInit(): void {
+    this.getUserData();
   }
 
+  public getUserData(): void {
+    let params: IQueryParams = {
+      orderBy: '"id"',
+      equalTo: `"${localStorage.getItem('localId')}"`,
+    };
+    this.userService.getData(params).subscribe((data: any) => {
+      this.user = Object.keys(data).map((a: any) => {
+        return data[a];
+      })[0];
+    });
+  }
 }
